27cdc7944c722a4a7ad93386eb3502059e2bf4cc
aleksandrtomash <tombasket508@gmail.com>
2022-01-21 14:09:02 +0200

    to work

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
 
-let res = function calculator(a, b , action)
-{
-if (action == 1)  return a + b;
-else if (action == 2)  return a - b;
-else if (action == 3) return a * b;
-else if (action == 4)
-{
-	if (b != 0) return a / b;
-	else
-		console.log("ERROR: divide by zero");
+const arHw = [13, 28, 4, 15, 25, -10, 40, 17, 27];
+arHw.sort(function(a,b){
+    if(a % 2 !=0 && b % 2 != 0) return b - a;
+    if(a % 2 ==0 && b % 2 == 0) return a - b;
+    if(a % 2 !=0 && b % 2 == 0) return 1;
+    if(a % 2 ==0 && b % 2 != 0) return -1;
+})
+console.log(arHw);
+
+////////////////////////////////////// Task 2
+function matrixTransp(matrix) {
+    const newMatrix = [];
+
+    for (let i = 0; i < matrix[0].length; i++) {
+        newMatrix.push([]);
+            for (let j = 0; j < matrix.length; j++) {
+                newMatrix[i].push(matrix[j][i]);
+            }
+    }
+    return newMatrix;
 }
-else
-	console.log("ERROR: Wrong action");
-    
-    }
-    console.log(res(10,0,4));
-  
+displayMatrix(matrixTransp([[1,2], [3,4], [5,6]]));
 
-function sum(num){
-   return function(c,d){
-   return num + c + d;
-    }
-   }
 
-console.log(sum(5)(10,3));
+function displayMatrix(matrix) {
+    for (let i = 0; i < matrix.length; i++) {
+            let row = '';
+            for (let j = 0; j < matrix[i].length; j++) {
+                    row = row + matrix[i][j] + '   '
+            }
+            console.log(row);
+    }  
+}
