e1f56febe38f84bab096366f47639c82c0e1f235
aleksandrtomash <tombasket508@gmail.com>
2022-01-21 11:45:06 +0200

    cw13

--- app.js
+++ app.js
@@ -1,79 +1,57 @@
-/******************ARRAYS************** */
+///////////////////ForEach////////////////////
 
-const ar = [2, 100, 9, 80];
-// ar.sort(function(a, b){   //// если не передать ничего . жс выводит как строки
-//         return a - b; 
-// });     
-ar.sort((a, b) => a - b);              //     то же самое, просто лругой синтаксис
-//ar.sort((a, b) => {return a- b});
+const ar = [-10, 50, -12, 80, 40];
+ar.push(70);
+// str - "-10#50#-12#80#40#70#"
 
-console.log(`ar is ${ar.toString()}`);
-/**************HomeWork #12 */
-const arHw = [13, 28, 4 , 15, 25, -10, 40, 17, 27];
-// expected [-10,4,28,40  ,  27,25,17,15,13]
-// comporator returns < 0 if fierst less than second, > 0 if first greater than second, == 0 if first equals second
+// let str = '';
+// ar.forEach(function(n){                                     ******1*******
+//     str += n + '#';
+// })
+//str = str.substring(0, str.length - 1);
 
-// adding elements/s at and an array
+// str - "-10#50#-12#80#40#70"
+// const lastSharpeIndex = str.lastIndexOf('#')
+// const str1 = str.substring(0, lastSharpeIndex);
+// const str2 = str.substring(lastSharpeIndex + 1);
+// str = str1 + str2;
 
-ar[ar.length] = 200;
-console.log(ar);
-ar.push(300, 25)
-console.log(ar);
-                        
-/*******НАЧАЛО******** */
+/*******************solution based on forEach from second number *********2**********/ 
+// const ar1 = ar.slice(1);
+// let str = '' + ar[0];
+// ar1.forEach(n => str += '#' + n);
+// console.log(str);
 
-const ar1 = [-8, 30, -57];
-//ar.push(ar1); there will be added array but not separated numbers
-// if you want to add separated numbers you should apply a spread operator
-ar.push(...ar1);
-console.log(ar);
+/***************printing out number of element , element , length********** */
+// 1 of 5 = -10
+ar.forEach((n, i, a) => console.log(`${i + 1} of ${a.length} - ${n}`));
+/****************HW****Task 1******** */
+// write function myForEach(array,calback - function)
+// array - being iterated array
+// calback-function   -  function that is will be called for each element of array
+// calback-function should take 3 arguments : current element, current index,  being iterated array
+//example  ar1.forEach(n => str += '#' + n);
 
-/***********КОНЕЦ********** */
+// myForEach(array, n => str += '#' + n);
 
-// for adding elements at beginning of  an array => "unshift"
-// everysing has been said "push" will  work "unshift"
-const ar2 = [27, 35];
-ar.unshift(...ar2);
-console.log(ar);
+/******************************"MAP"******************** */
+// USE CASE OF APPLYING METHOD MAP : YOU WHANT TO CREATE NEW ARRAY WITH ELEMENTS THAT ARE RECIVED AS RESULT OF SOM
+// EXAMPLE : 
+const ar2 = ar.map(n => n * 2);
+console.log(ar2)
 
-/**********СЕРЕДИНА******** */
+//const ar3 = ar.map(n => `<li>${n}</li>`);
+const ar3 = ar.map((n ,i, a ) => `<li>${i + 1} of ${a.length} - ${n}</li>`);
+console.log(ar3)
 
-// METHOD "SPLICE" for inserting or replacing any elements in any plase
 
-// first argument - index for inserting or replasing.
-// second arg - number of the deleted elements (if 0 than splice will be applied)
-// third - designares 
+// task 2
+// myMap (kak forEach), 
 
-ar.splice(2, 0, 1, 2);
-console.log(ar);
 
-// REMOOVING/DELETING
 
-let el = ar.pop();
-console.log(ar);
-
-el = ar.shift();  // remoovs first element and return it
-console.log(ar);
-
-el = ar.slice(3,5); // remoove 5 elements beginning from the  one at index 3 
-console.log(ar);
-
-// includes, indexOf
-
-// two dimensional arrays
-
-const matrix1 = [[1, 2, 3,], [4, 5, 6], [7, 8, 9], [10, 11, 12]];
-const matrix2 = [[100, 20], [50, -5], [34, 28]];
-
-function displayMatrix(matrix){
-    let row = " ";
-    for(let i = 0; i < matrix.length; i++){
-        for(let j = 0; j < matrix[i.length]; j++){
-            row = row + matrix[i][j] + "  ";
-            
-        }
-        console.log(row);
-    }
-}
-//displayMatrix( matrix1);
-displayMatrix( matrix1);
+ let arr = ['20', 22, ' year whan korona dai'];
+ arr.forEach((function(e, i , ar) => console.log( `${e} + ${i} + ${arr}`));
+ 
+    
+    
