bfce4d620645bd8cc3be99ccd8dc5ea1b142ec9d
aleksandrtomash <tombasket508@gmail.com>
2022-01-21 13:13:23 +0200

    1

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 
 
-let arr = ['20', 22, ' year whan korona dai'];
-arr.forEach((function(e, i , ar) => console.log( `${e} + ${i} + ${arr}`));
+const arr = [10, 20, 30, 40, 50];
+function myForEach(el, ind, arr){
+
+ arr[ind] = el + '#';
+}
+myForEach(console.log(arr));
