1f34761cf94bd53450f5aa0f1321c81145832cf6
aleksandrtomash <tombasket508@gmail.com>
2022-01-21 11:48:36 +0200

    hw13

--- app.js
+++ app.js
@@ -1,26 +1,4 @@
 
-let res = function calculator(a, b , action)
-{
-if (action == 1)  return a + b;
-else if (action == 2)  return a - b;
-else if (action == 3) return a * b;
-else if (action == 4)
-{
-	if (b != 0) return a / b;
-	else
-		console.log("ERROR: divide by zero");
-}
-else
-	console.log("ERROR: Wrong action");
-    
-    }
-    console.log(res(10,0,4));
-  
 
-function sum(num){
-   return function(c,d){
-   return num + c + d;
-    }
-   }
-
-console.log(sum(5)(10,3));
+let arr = ['20', 22, ' year whan korona dai'];
+arr.forEach((function(e, i , ar) => console.log( `${e} + ${i} + ${arr}`));
