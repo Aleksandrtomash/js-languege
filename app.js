4cf2a21e0db507991e6b1d8f8bbab1f4300d768b
aleksandrtomash <tombasket508@gmail.com>
2022-01-27 19:10:47 +0200

    hw + reduce

--- app.js
+++ app.js
@@ -27,8 +27,30 @@ const ar1 = [10, 20, 30, 40, 50];
 
     /////////myFiltr/////////
 
+
+    function myFiltr(array, callbackFunc){
+        const res = [];
+
+    }
     const arFiltr = [-2, 5, 8, 3, 9, -12, 20, 0];
 
-    const newArFiltr = arFiltr.filter(function(item, i, arr){
+    const newArFiltr = arFiltr.filter(function(item, i, arr){ 
 
     });
+
+/////////////myReduce////////////////
+
+    function myReduce(array,callbackReduce, initial){
+        if(initial == undefined){
+            initial = array[0];
+            array = array.slice(1);
+        }
+        let res = initial;
+        function forEahCallback(n,i,a){
+            res = callbackReduce(res,n,i,a);
+        }
+        myForEach(array,(n, i, a) => res = callbackReduce(res,n,i,a));
+        return res;
+    }
+    let res = myReduce(ar1,(res,cur) => res + cur, 0);
+    console.log(res);
