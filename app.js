2ae9d8459f5dac1eaca6f630cc52298401f4038a
aleksandrtomash <tombasket508@gmail.com>
2022-02-06 20:15:02 +0200

    cw#17

--- app.js
+++ app.js
@@ -1,119 +1,95 @@
-const circle = {radius: 20, square: function() {
-    return 3.14 * (this.radius ** 2)
-}, perimeter: () => 2 * 3.14 * this.radius,
-toString: function(){
-    return `radious of this circle is ${this.radius}`
-}};
-console.log(`square=${circle.square()}, perimeter=${circle.perimeter()}`); 
-//circle.square will be converted to square(circle) where circle as argument value,
-// "this" is the hidden parameter in the function 
-// in any arrow function there is no "this" 
-function square(circle) {
-    return 3.14 * (circle.radius ** 2);
+class Person {
+        #id;
+        #name;
+        constructor(id, name){
+                this.#id = id;
+                this.#name = name
+        }
+        getId(){
+               return this.#id;
+        }
+        getname(){
+            
+                return this.#name
+        }
+        toString(){
+                return  `id: ${this.#id}; name: ${this.#name}`
+                }
 }
-console.log(`circle: ${circle}`)
-const circle1 = {radius: 20,  perimeter: function() {
-            return 2 * 3.14 * this.radius}
-    ,
-    toString: function(){
-            return `radious of this circle is ${this.radius}`
-    }};
-    //circle1.square(); error because the method square is not defined inside object circle1
-    function Circle(radius) {
-            this.radius = radius;
-    }
-    Circle.prototype.square = function() {
-            return 3.14 * (this.radius ** 2);  
-    }
-    Circle.prototype.perimeter = function() {
-            return 2 * 3.14 * this.radius
-    }
-    Circle.prototype.toString = function() {
-            return `radius of this circle is ${this.radius}`
-    }
-    const circle10 = new Circle(10);
-    /*******************************************************HW #16 definition task 1 */
-//Write constructor Deferred
+const person = new Person(123, `Moshe`);
+console.log(`my person is ${person}`);
 
- console.log(`HW ================== 16 Task 1 =============================== `)
+class Employee extends Person{
+       #salary;
+       constructor(id, name, salary){
+            super(id, name); // call the constractor of the class Person
+            this.#salary = salary;
+       }
+       computeSalary(){
+               return this.#salary;
+       }
+       toString(){
+               return super.toString + ` salary: ${this.computeSalary()}`
+       }
+} 
+const person2 = new Employee(124, "Sara",5000);
+console.log(`person2 is ${person2}`)
+console.log(typeof(person2));
+console.log(person2.constructor.name); // tolko tak polu4aem imya konstruktora v js
 
-function Deferred(){
-        this.functions = [] 
-};
-Deferred.prototype.then = function(thenFn){
-        this.functions.push(thenFn);
-}        
-Deferred.prototype.resolve = function (res){
-        this.functions.forEach(fn => res = fn(res));
+class Child extends Person{
+        #kindergarten
+        constructor(id, name, kindergarten){
+                super(id, name);
+                this.#kindergarten = kindergarten;
+        }
+        getKindergarten(){
+                return this.#kindergarten;
+        }
+        toString(){
+                return `${super.toString()} kindergarten: ${this.#kindergarten}`
+        }
 }
-     const d = new Deferred()
+const person3 = new Child(125, `Yakob`, `Shalom`);
+console.log(` person3 is: ${person3}`);
 
- d.then(function(res){ console.log('1 ', res); return 'a'; });
-
- d.then(function(res){ console.log('2 ', res); return 'b'; });
-
- d.then(function(res){ console.log('3 ', res); return 'c'; });
- d.resolve('hello');
-//Output: 
-//1 hello
-//2 a
-//3 b
-/********************************************************* */
-/*******************************************HW #16 definition task2 */
-
-
-//write constructor MyArray
-
-
-//const myArray = new MyArray(10);
-//myArray.get(index) - result 10
-//write method get getting an index value and returning common value
-// (set in constructor)
-//myArray.set(index, value); 
-//write method set that sets a given value at a given index
-//myArray.setValue(value) - sets new value in all elements of myArray
-//Example:
-
-console.log(`HW ================== 16 Task 2 =================================== `)
-
-function MyArray(initialValue) {
-        this.value = initialValue;
-        this.array = [];
+class WageEmployee extends Employee{
+        #hours
+        #wage
+        constructor(id, name, salary, hours, wage){
+                 super(id, name, salary);
+                 this.#hours = hours;
+                 this.#wage = wage;
+        }
+        computeSalary(){
+                return super.computeSalary() + this.#hours * this.#wage;
+        }
+      
 }
-MyArray.prototype.setValue = function (value) {
-        this.value = value;
-        this.array = [];
+const person4 = new WageEmployee(126, `Asaf`, 1000, 10, 100);
+console.log(`person4 is: ${person4}`);
+
+/*********************************HW#17************************* */
+const person = [
+        new Child(100, `Olya`, `Shalom`),,
+        new Child(101, `Serega`, `boker`),
+        new Child(102, `Kolya`, `Shalom`),
+        new Employee(103, `Vasya`, 1000),
+        new WageEmployee(104, `Tolya`, 1000, 10, 100)
+]
+function countofPersonType(person,type){
+        // return count of persons of the given type
+        // Example: 
+        // countofPersonType(persons, `WageEmploee`)----> 1
 }
-MyArray.prototype.set = function (index, value) {
-        this.array[index] = value;
+function computeSalaryBudget(persons){
+        // returns total salary of all emploeee objects in the given array
+        //Example:
+        //computeSalaryBudget(persons)---> 3000
+        //
 }
-MyArray.prototype.get = function(index){
-        return this.array[index] ?? this.value; // ?? если  array[index] either underfined or null returns this.value
+function countChildrenGindergarten(persons,kindergarten){
+        // returns number of children in the given kindergarten
+        //
+        //countChildrenGindergarten(persons, shalom)---> 2
 }
- const myArray = new MyArray(10);
- console.log(`should be 10` ,myArray.get(100)) // displayed out 10
- myArray.set(100, 500)//sets 500 at index 100
- console.log(`should be 10`,myArray.get(200)) //displayed out 10
- console.log(`should be 500`,myArray.get(100)) //displayed out 500
- myArray.setValue(300);
- console.log(`should be 300`,myArray.get(100)) //displayed out 300
- console.log(`should be 10`,myArray.get(200)) //displayed out 300
-
-
-
-/***************************************************************************** */
-Array.prototype.filter = function(callbackPredicate) {
-    console.log('Tel-Ran copyright')
-    const res = []
-   this.forEach((n, i, a) => callbackPredicate(n, i, a) && res.push(n));
-   return res;
-}
-const ar = [1, 2, 4, 5, 100];
-ar.filter(n => n % 2 !== 0).forEach(n => console.log(n));
-
-/************************************************************************ */
-
-//kak opredelaem dlinu massiva:
-const arr1 = [];
-arr1[1000000000] = 10;
-console.log(`length of arr1 is ${arr1.length}`)
